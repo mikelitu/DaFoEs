10f25ec6872abf6f28310e0a847fed9fb0b2999d
mikelitu <mikelitubiomed@gmail.com>
2023-05-05 21:25:23 +0800

    make the depth loading optional

--- test.py
+++ test.py
@@ -24,17 +24,18 @@ parser.add_argument("--train-type", type=str, default='random', help='The traini
 parser.add_argument("--data-dir", type=str, help="Directory of the data")
 parser.add_argument("--save-dir", default='results', type=str, help='Save directory for the metrics and predictions')
 parser.add_argument("--save", action='store_true', help='Save metrics and predictions for further analysis')
+parser.add_argument("--include-depth", action='store_true')
 
-
-def load_from_folder(folder: Path, limit: int = 1000, architecture: str = 'cnn', recurrency_size: int = 1):
+def load_from_folder(folder: Path, include_depth: bool, limit: int = 1000, recurrency_size: int = 1):
     mu_labels = np.load('labels_mean.npy')
     std_labels = np.load('labels_std.npy')
     folder = Path(folder)
     rgb_folder = folder/"RGB_frames"
-    depth_folder = folder/"Depth_frames"
-
     images = sorted(rgb_folder.files('*.png'))
-    depths = sorted(depth_folder.files('*.png'))
+
+    if include_depth:
+        depth_folder = folder/"Depth_frames"
+        depths = sorted(depth_folder.files('*.png'))
 
     labels = np.array(pd.read_csv(folder/'labels.csv'))
     norm_labels = (labels - mu_labels) / (std_labels - 1e-10)
@@ -55,7 +56,12 @@ def load_from_folder(folder: Path, limit: int = 1000, architecture: str = 'cnn',
             
         data = {}
         img = [im for im in images[i:i+recurrency_size]]
-        depth = [d for d in depths[i:i+recurrency_size]]
+
+        if include_depth:
+            depth = [d for d in depths[i:i+recurrency_size]]
+        else:
+            depth = None
+
         state = [np.mean(norm_labels[i*nlabels+a:(i*nlabels+a) + step, :26], axis=0) for a in range(recurrency_size)]
         force = [np.mean(norm_labels[i*nlabels+a:(i*nlabels+a) + step, -6:-3], axis=0) for a in range(recurrency_size)]
         data['img'] = img
@@ -67,7 +73,7 @@ def load_from_folder(folder: Path, limit: int = 1000, architecture: str = 'cnn',
     return inputs
 
 
-def load_test_experiment(architecture: str, include_state: bool = True,  train_mode: str = "random"):
+def load_test_experiment(architecture: str, include_depth: bool, include_state: bool = True,  train_mode: str = "random"):
     train_modes = ["random", "color", "geometry", "structure", "stiffness", "position"]
     assert architecture.lower() in ["vit", "cnn", "vit-base", "cnn-bam", "vit-dist", "fc", "rnn", "rnn-bam"], "The architecture has to be either 'vit' or 'cnn', '{}' is not valid".format(architecture)
     assert train_mode in train_modes, "'{}' is not an available training mode. The available training mode are: {}".format(train_mode, train_modes)
@@ -119,25 +125,25 @@ def load_test_experiment(architecture: str, include_state: bool = True,  train_m
         print("LOADING EXPERIMENT [=>   ]")
         print("Chosen model is ResNet50 (CNN) {}".format("vision+state" if include_state else "vision only"))
         # Choosing the model
-        model = ForceEstimatorVS(rs_size=26, num_layers=50, pretrained=False, att_type='BAM', include_depth=True) if include_state else ForceEstimatorV(num_layers=50, pretrained=False, att_type='BAM', include_depth=True)
+        model = ForceEstimatorVS(rs_size=26, num_layers=50, pretrained=False, att_type='BAM', include_depth=include_depth) if include_state else ForceEstimatorV(num_layers=50, pretrained=False, att_type='BAM', include_depth=include_depth)
 
     elif architecture.lower() == 'cnn':
         print("LOADING EXPERIMENT [=>   ]")
         print("Chosen model is ResNet50 (CNN) {}".format("vision+state" if include_state else "vision only"))
         # Choosing the model
-        model = ForceEstimatorVS(rs_size=26, num_layers=50, pretrained=False, include_depth=True) if include_state else ForceEstimatorV(num_layers=50, pretrained=False, include_depth=True)
+        model = ForceEstimatorVS(rs_size=26, num_layers=50, pretrained=False, include_depth=include_depth) if include_state else ForceEstimatorV(num_layers=50, pretrained=False, include_depth=include_depth)
 
     elif architecture.lower() == 'rnn':
         print("LOADING EXPERIMENT [=>   ]")
         print("Chosen model is ResNet50 (RNN) {}".format("vision+state" if include_state else "vision only"))
         # Choosing the model
-        model = RecurrentCNN(num_layers=50, pretrained=False, include_depth=True) if include_state else RecurrentCNN(num_layers=50, pretrained=False, include_depth=True)
+        model = RecurrentCNN(num_layers=50, pretrained=False, include_depth=include_depth)
 
     elif architecture.lower() == 'rnn-bam':
         print("LOADING EXPERIMENT [=>   ]")
         print("Chosen model is ResNet50 (RNN) {}".format("vision+state" if include_state else "vision only"))
         # Choosing the model
-        model = RecurrentCNN(num_layers=50, pretrained=False, include_depth=True, att_type='BAM') if include_state else RecurrentCNN(num_layers=50, pretrained=False, include_depth=True, att_type='BAM')
+        model = RecurrentCNN(num_layers=50, pretrained=False, include_depth=include_depth, att_type='BAM')
 
     if architecture.lower() in ["rnn", "rnn-bam"]:
         recurrency_size = 5
@@ -147,10 +153,14 @@ def load_test_experiment(architecture: str, include_state: bool = True,  train_m
     # Find the corresponding checkpoint
     print("LOADING EXPERIMENT [==>  ]")
     checkpoints_root = Path('/home/md21local/mreyzabal/checkpoints/img2force')
+    
     if architecture.lower() == 'fc':
         checkpoints = checkpoints_root/"{}/{}".format(architecture, train_mode)
+    elif not include_depth:
+        checkpoints = checkpoints_root/"{}/{}/{}_{}".format("rgb", architecture, "visu_state" if include_state else "visu", train_mode)
     else:
         checkpoints = checkpoints_root/"{}/{}_{}".format(architecture, "visu_state" if include_state else "visu", train_mode)
+    
     print('The checkpoints are loaded from: {}'.format(sorted(checkpoints.dirs())[-1]))   
     checkpoint_dir = sorted(checkpoints.dirs())[-1]/'checkpoint.pth.tar'
     print("LOADING EXPERIMENT [===> ]")
@@ -178,22 +188,22 @@ def load_test_experiment(architecture: str, include_state: bool = True,  train_m
     data_dir = root_dir/test_data_dir
     print('Loading data from {}'.format(data_dir))
 
-    test_data = load_from_folder(data_dir, 450, recurrency_size=recurrency_size)
+    test_data = load_from_folder(data_dir, limit=450, recurrency_size=recurrency_size, include_depth=include_depth)
 
     print('Loading data from shared dataset {}'.format(root_dir/share_dir))
-    share_data = load_from_folder(root_dir/share_dir, 450, recurrency_size=recurrency_size)
+    share_data = load_from_folder(root_dir/share_dir, limit=450, recurrency_size=recurrency_size, include_depth=include_depth)
 
     return model, test_data, share_data
 
 
-def run_test_experiment(architecture: str, transforms, include_state: bool = True, train_mode: str = "random"):
+def run_test_experiment(architecture: str, transforms: Compose, include_depth: bool, include_state: bool = True, train_mode: str = "random"):
 
     test_predictions, shared_predictions = [], []
     test_metrics, shared_metrics = [], []
     test_forces, shared_forces = [], []
 
     # Loading the necessary data
-    model, test_data, shared_data = load_test_experiment(architecture, include_state, train_mode)
+    model, test_data, shared_data = load_test_experiment(architecture, include_depth=include_depth, include_state=include_state, train_mode=train_mode)
 
     model.to(torch.device("cuda"))
 
@@ -202,10 +212,15 @@ def run_test_experiment(architecture: str, transforms, include_state: bool = Tru
     for j in tqdm(range(len(test_data))):
         inp = test_data[j]
         imgs = [load_as_float(img) for img in inp['img']]
-        depths = [load_as_float(d) for d in inp['depth']]
+        if include_depth:
+            depths = [load_as_float(d) for d in inp['depth']]
+        else:
+            depths = inp['depth']
         imgs, depths, state, force = transforms(imgs, depths, inp['state'], inp['force'])
-        depths = [process_depth(d) for d in depths]
-        imgs = [torch.cat([img, depth], axis=0) for img, depth in zip(imgs, depths)]
+        if include_depth:
+            depths = [process_depth(d) for d in depths]
+            imgs = [torch.cat([img, depth], axis=0) for img, depth in zip(imgs, depths)]
+        
         imgs = [img.unsqueeze(0).cuda() for img in imgs]
         state = torch.from_numpy(state).float().cuda()
         force = torch.from_numpy(force).float().cuda()
@@ -229,21 +244,25 @@ def run_test_experiment(architecture: str, transforms, include_state: bool = Tru
         rmse = torch.sqrt(((force - pred_force) ** 2).mean())
 
         test_metrics.append(rmse.item())
-        test_forces.append([f for f in force.squeeze(0).detach().cpu().numpy()])
-        test_predictions.append([f for f in pred_force.squeeze(0).detach().cpu().numpy()] if include_state else pred_force.squeeze(0).detach().cpu().numpy())
-    
+        test_forces.append([f for f in force.detach().cpu().numpy()])
+        test_predictions.append([f for f in pred_force.detach().cpu().numpy()])
+
     test_metrics = np.array(test_metrics)
-    test_forces = np.array(test_forces).reshape(-1, 3) if include_state else np.array(test_forces).mean(axis=0)
-    test_predictions = np.array(test_predictions).reshape(-1, 3) if include_state else np.array(test_predictions)
+    test_forces = np.array(test_forces).reshape(-1, 3)
+    test_predictions = np.array(test_predictions).reshape(-1, 3)
 
 
     for b in tqdm(range(len(shared_data))):
         inp = test_data[b]
         imgs = [load_as_float(img) for img in inp['img']]
-        depths = [load_as_float(d) for d in inp['depth']]
+        if include_depth:
+            depths = [load_as_float(d) for d in inp['depth']]
+        else:
+            depths = inp['depth']
         imgs, depths, state, force = transforms(imgs, depths, inp['state'], inp['force'])
-        depths = [process_depth(d) for d in depths]
-        imgs = [torch.cat([img, depth], axis=0) for img, depth in zip(imgs, depths)]
+        if include_depth:
+            depths = [process_depth(d) for d in depths]
+            imgs = [torch.cat([img, depth], axis=0) for img, depth in zip(imgs, depths)]
         imgs = [img.unsqueeze(0).cuda() for img in imgs]
         state = torch.from_numpy(state).float().cuda()
         force = torch.from_numpy(force).float().cuda()
@@ -267,12 +286,12 @@ def run_test_experiment(architecture: str, transforms, include_state: bool = Tru
         rmse = torch.sqrt(((force - pred_force) ** 2).mean())
 
         shared_metrics.append(rmse.item())
-        shared_forces.append([f for f in force.squeeze(0).detach().cpu().numpy()])
-        shared_predictions.append([f for f in pred_force.squeeze(0).detach().cpu().numpy()] if include_state else pred_force.squeeze(0).detach().cpu().numpy())
+        shared_forces.append([f for f in force.detach().cpu().numpy()])
+        shared_predictions.append([f for f in pred_force.detach().cpu().numpy()])
     
     shared_metrics = np.array(shared_metrics)
-    shared_forces = np.array(shared_forces).reshape(-1, 3) if include_state else np.array(shared_forces).mean(axis=0)
-    shared_predictions = np.array(shared_predictions).reshape(-1, 3) if include_state else np.array(shared_predictions)
+    shared_forces = np.array(shared_forces).reshape(-1, 3)
+    shared_predictions = np.array(shared_predictions).reshape(-1, 3)
     # shared_dtw = [dtw(f, p) for f, p in zip(shared_forces, shared_predictions)]
 
     results = {'test_rmse': test_metrics, 'test_gt': test_forces, 'test_pred': test_predictions,
@@ -283,8 +302,8 @@ def run_test_experiment(architecture: str, transforms, include_state: bool = Tru
 
 def save_results(args, results):
     root_dir = Path('/home/md21local/img2force')
-    print("The results will be saved at: {}/{}".format(root_dir, args.save_dir))
-    save_dir = root_dir/args.save_dir
+    print("The results will be saved at: {}/{}/{}".format(root_dir, args.save_dir, "rgbd" if args.include_depth else "rgb"))
+    save_dir = root_dir/args.save_dir/"{}".format("rgbd" if args.include_depth else "rgb")
     save_dir.makedirs_p()
     f = open(save_dir/'{}_{}_{}.pkl'.format(args.architecture.lower(), "state" if args.state else "visu", args.train_type), 'wb')
     pickle.dump(results, f)
@@ -306,7 +325,7 @@ def main():
         normalize
     ])
 
-    results = run_test_experiment(args.architecture, transforms, args.state, args.train_type)
+    results = run_test_experiment(args.architecture, transforms=transforms, include_depth=args.include_depth, include_state=args.state, train_mode=args.train_type)
     if args.save:
         save_results(args, results)
 
