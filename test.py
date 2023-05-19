872fcea54efe089ed16fa0460b32c410ddec2f48
mikelitu <mikelitubiomed@gmail.com>
2023-05-19 19:06:42 +0100

    start rewriting of the testing file

--- test.py
+++ test.py
@@ -1,149 +1,42 @@
 import torch
-from datasets.augmentations import Compose, CentreCrop, SquareResize, Normalize, ArrayToTensor
-from models.force_estimator_2d import ForceEstimatorV, ForceEstimatorVS, ForceEstimatorS, RecurrentCNN
-from models.force_estimator_transformers import ViT
-from models.force_estimator_transformers_base import BaseViT
+from datasets.augmentations import Compose, CentreCrop, SquareResize, BrightnessContrast, Normalize, ArrayToTensor
+from models.force_estimator import ForceEstimator
 from path import Path
-from datasets.vision_state_dataset import load_as_float, load_depth, process_depth
+from datasets.test_dataset import TestDataset
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import argparse
 from train import vit_predict_force_state, vit_predict_force_visu
-from train_cnn import cnn_predict_force_state, cnn_predict_force_visu
 import pickle
 import os
 from utils import dtw, frdist
 
 parser = argparse.ArgumentParser(description="Script to test the different models for ForceEstimation variability",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--architecture", choices=['cnn', 'vit', 'cnn-bam', 'vit-base', 'fc', 'rnn', 'rnn-bam'], default='vit', help='The chosen architecture to test')
+parser.add_argument("--architecture", choices=['cnn', 'vit', 'fc'], default='vit', help='The chosen architecture to test')
 parser.add_argument("--state", action='store_true', help='Include the state')
 parser.add_argument("--train-type", type=str, default='random', help='The training type of the chosen model')
 parser.add_argument("--data-dir", type=str, help="Directory of the data")
 parser.add_argument("--save-dir", default='results', type=str, help='Save directory for the metrics and predictions')
 parser.add_argument("--save", action='store_true', help='Save metrics and predictions for further analysis')
+parser.add_argument("--recurrency", action='store_true')
 parser.add_argument("--include-depth", action='store_true')
-
-def load_from_folder(folder: Path, include_depth: bool, limit: int = 1000, recurrency_size: int = 1):
-    mu_labels = np.load('labels_mean.npy')
-    std_labels = np.load('labels_std.npy')
-    folder = Path(folder)
-    rgb_folder = folder/"RGB_frames"
-    images = sorted(rgb_folder.files('*.png'))
-
-    if include_depth:
-        depth_folder = folder/"Depth_frames"
-        depths = sorted(depth_folder.files('*.png'))
-
-    labels = np.array(pd.read_csv(folder/'labels.csv'))
-    norm_labels = (labels - mu_labels) / (std_labels - 1e-10)
-
-    inputs = []
-    nlabels = labels.shape[0] // len(rgb_folder.files('*.png'))
-    step = 7
-
-    breaking_limit = min(len(rgb_folder.files('*.png')) - 40, limit)
-
-    print('Loading a total of {} images'.format(breaking_limit - 20))
-
-    for i in range(len(images)):
-        if i == breaking_limit-recurrency_size:
-            break
-        if i < 20:
-            continue
-            
-        data = {}
-        img = [im for im in images[i:i+recurrency_size]]
-
-        if include_depth:
-            depth = [d for d in depths[i:i+recurrency_size]]
-        else:
-            depth = None
-
-        state = [np.mean(norm_labels[i*nlabels+a:(i*nlabels+a) + step, :26], axis=0) for a in range(recurrency_size)]
-        force = [np.mean(norm_labels[i*nlabels+a:(i*nlabels+a) + step, -6:-3], axis=0) for a in range(recurrency_size)]
-        data['img'] = img
-        data['depth'] = depth
-        data['state'] = np.array(state)
-        data['force'] = np.array(force)
-        inputs.append(data)
-    
-    return inputs
+parser.add_argument("--chua", action="store_true")
+parser.add_argument("--att-type", default=None, help="Additional attention values")
 
 
-def load_test_experiment(architecture: str, include_depth: bool, include_state: bool = True,  train_mode: str = "random"):
+def load_test_experiment(architecture: str, include_depth: bool, include_state: bool = True, recurrency: bool = False,  train_mode: str = "random", att_type: str = None):
     train_modes = ["random", "color", "geometry", "structure", "stiffness", "position"]
-    assert architecture.lower() in ["vit", "cnn", "vit-base", "cnn-bam", "vit-dist", "fc", "rnn", "rnn-bam"], "The architecture has to be either 'vit' or 'cnn', '{}' is not valid".format(architecture)
+    assert architecture.lower() in ["vit", "cnn", "fc"], "The architecture has to be either 'vit' or 'cnn', '{}' is not valid".format(architecture)
     assert train_mode in train_modes, "'{}' is not an available training mode. The available training mode are: {}".format(train_mode, train_modes)
-    
-    
-    
-    print("Experiment variables: architecture -> {}, include_state -> {} & train_mode -> {}".format(architecture, include_state, train_mode))
 
-    if architecture.lower() == "vit":
-        print("LOADING EXPERIMENT [=>   ]")
-        print("Chosen model is Vision Transformer (ViT) with token sampling {}".format("vision+state" if include_state else "vision only"))
-        # Choosing the model
-        model = ViT(
-            image_size = 256,
-            patch_size = 16,
-            num_classes = 3,
-            dim = 1024,
-            depth = 6,
-            heads = 16,
-            mlp_dim = 2048,
-            dropout = 0.1,
-            emb_dropout = 0.1,
-            max_tokens_per_depth = (256, 128, 64, 32, 16, 8),
-            state_include = include_state
-        )
-    
-    elif architecture.lower() == 'vit-base' or architecture.lower() == 'vit-dist':
-        print("LOADING EXPERIMENT [=>   ]")
-        print("Chosen model is Vision Transformer (ViT) {}".format("vision+state" if include_state else "vision only"))
-        model = BaseViT(
-            image_size = 256,
-            patch_size = 16,
-            num_classes = 3,
-            dim = 1024,
-            depth = 6,
-            heads = 16,
-            mlp_dim = 2048,
-            dropout = 0.1,
-            emb_dropout = 0.1,
-            state_include = include_state
-        )
-    
-    elif architecture.lower() == 'fc':
-        print("LOADINF EXPERIMENT [=>  ]")
-        print("Chosen model is FC only state")
-        model = ForceEstimatorS(rs_size=26)
-
-    elif architecture.lower() == 'cnn-bam':
-        print("LOADING EXPERIMENT [=>   ]")
-        print("Chosen model is ResNet50 (CNN) {}".format("vision+state" if include_state else "vision only"))
-        # Choosing the model
-        model = ForceEstimatorVS(rs_size=26, num_layers=50, pretrained=False, att_type='BAM', include_depth=include_depth) if include_state else ForceEstimatorV(num_layers=50, pretrained=False, att_type='BAM', include_depth=include_depth)
-
-    elif architecture.lower() == 'cnn':
-        print("LOADING EXPERIMENT [=>   ]")
-        print("Chosen model is ResNet50 (CNN) {}".format("vision+state" if include_state else "vision only"))
-        # Choosing the model
-        model = ForceEstimatorVS(rs_size=26, num_layers=50, pretrained=False, include_depth=include_depth) if include_state else ForceEstimatorV(num_layers=50, pretrained=False, include_depth=include_depth)
-
-    elif architecture.lower() == 'rnn':
-        print("LOADING EXPERIMENT [=>   ]")
-        print("Chosen model is ResNet50 (RNN) {}".format("vision+state" if include_state else "vision only"))
-        # Choosing the model
-        model = RecurrentCNN(num_layers=50, pretrained=False, include_depth=include_depth)
-
-    elif architecture.lower() == 'rnn-bam':
-        print("LOADING EXPERIMENT [=>   ]")
-        print("Chosen model is ResNet50 (RNN) {}".format("vision+state" if include_state else "vision only"))
-        # Choosing the model
-        model = RecurrentCNN(num_layers=50, pretrained=False, include_depth=include_depth, att_type='BAM')
+    model = ForceEstimator(architecture,
+                           recurrency=recurrency,
+                           pretrained=False,
+                           include_depth=include_depth,
+                           att_type=att_type)
 
     if architecture.lower() in ["rnn", "rnn-bam"]:
         recurrency_size = 5
@@ -156,13 +49,11 @@ def load_test_experiment(architecture: str, include_depth: bool, include_state: 
     
     if architecture.lower() == 'fc':
         checkpoints = checkpoints_root/"{}/{}".format(architecture, train_mode)
-    elif not include_depth:
-        checkpoints = checkpoints_root/"{}/{}/{}_{}".format("rgb", architecture, "visu_state" if include_state else "visu", train_mode)
     else:
-        checkpoints = checkpoints_root/"{}/{}_{}".format(architecture, "visu_state" if include_state else "visu", train_mode)
+        checkpoints = checkpoints_root/"{}/{}/{}_{}".format("rgbd" if include_depth else "rgb", architecture, "visu_state" if include_state else "visu", train_mode)
     
     print('The checkpoints are loaded from: {}'.format(sorted(checkpoints.dirs())[-1]))   
-    checkpoint_dir = sorted(checkpoints.dirs())[-1]/'checkpoint.pth.tar'
+    checkpoint_dir = sorted(checkpoints.dirs())[-1]/'model_best.pth.tar'
     print("LOADING EXPERIMENT [===> ]")
     print("Loading weights...")
     checkpoint = torch.load(checkpoint_dir)
@@ -176,8 +67,8 @@ def load_test_experiment(architecture: str, include_depth: bool, include_state: 
     test_dirs = {
         'random': 'DD_P_D_R_2_1',
         'color': 'EE_P_D_R_L1',
-        'geometry': 'D_S_S_P_L2',
-        'stiffness': 'D_P_S_R_R1',
+        'geometry': 'DE__S_R_2_3',
+        'stiffness': 'DE_P_D_R_3_4',
         'structure': 'ED_P_D_R_C',
         'position': 'E_S_S_R_R2'
     }
