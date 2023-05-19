fe0db48585c2b5ad7943a31899f04fc15f6a29fa
mikelitu <mikelitubiomed@gmail.com>
2023-05-19 19:06:20 +0100

    update saving file naming

--- train.py
+++ train.py
@@ -1,23 +1,18 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 from tqdm import tqdm
 from torch.utils.data import DataLoader
-from losses import RMSE, EuclideanDist, GDL
 import argparse
-import datetime
 import time
 import csv
 import numpy as np
 from datasets import augmentations
-from utils import save_checkpoint
+from utils import save_checkpoint, create_saving_dir
 from tensorboardX import SummaryWriter
 from path import Path
 from logger import TermLogger, AverageMeter
-from models.force_estimator_transformers import ViT
-from models.force_estimator_transformers_base import BaseViT
-from models.force_estimator_2d import ForceEstimatorV, ForceEstimatorVS, RecurrentCNN
+from models.force_estimator import ForceEstimator
 from models.recorder import Recorder
 from datasets.vision_state_dataset import VisionStateDataset
 from datasets.chua_dataset import ZhongeChuaDataset
@@ -27,8 +22,7 @@ parser = argparse.ArgumentParser(description='Vision and roboto state based forc
 
 parser.add_argument('data', metavar='DIR', help='path to dataset')
 parser.add_argument('--type', default='vs', choices=['v', 'vs'], type=str, help='model type it can be vision only (v) or vision and state (vs)')
-parser.add_argument('--patch-size', default=32, type=int, metavar='N', help='size of the patches fed into the transformer')
-parser.add_argument('--token-sampling', action='store_true', help='sampled token size')
+parser.add_argument('--architecture', choices=['cnn', 'vit', 'fc'], type=str, help='')
 parser.add_argument('--epochs', default=200, type=int, metavar='N', help='number of total epochs to run')
 parser.add_argument('-b', '--batch-size', default=128, type=int, metavar='N', help='mini-batch size')
 parser.add_argument('--lr', '--learning-rate', default=2e-4, type=float, metavar='LR', help='initial learning rate')
@@ -46,7 +40,9 @@ parser.add_argument('-r', '--rmse-loss-weight', default=5.0, type=float, help='w
 parser.add_argument('-g', '--gd-loss-weight', default=0.5, type=float, help='weight for gradient difference loss')
 parser.add_argument('--train-type', choices=['random', 'geometry', 'color', 'structure', 'stiffness', "position"], default='random', type=str, help='training type for comparison')
 parser.add_argument('--chua', action='store_true')
-parser.add_argument('--include_depth', action='store_true')
+parser.add_argument('--include-depth', action='store_true')
+parser.add_argument('--att-type', default=None, help="adding attention layers to the network")
+parser.add_argument('--recurrency', action='store_true')
 
 best_error = -1
 n_iter = 0
@@ -58,10 +54,10 @@ torch.autograd.set_detect_anomaly(True)
 def main():
     global best_error, n_iter, device
     args = parser.parse_args()
-
-    timestamp = datetime.datetime.now().strftime("%m-%d-%H:%M")
     save_path = Path(args.name)
-    args.save_path = '/nfs/home/mreyzabal/checkpoints/{}/{}'.format('chua' if args.chua else 'img2force', 'vit' if args.token_sampling else 'vit-base')/save_path/timestamp
+    checkpoint_dir = Path('/nfs/home/mreyzabal/checkpoints')
+    args.save_path = create_saving_dir(checkpoint_dir, save_path, args.architecture, args.include_depth, args.chua, args.recurrency, args.att_type)
+    # args.save_path = '/nfs/home/mreyzabal/checkpoints/{}/{}'.format('chua' if args.chua else 'img2force', )/save_path/timestamp
     print('=> will save everything to {}'.format(args.save_path))
     args.save_path.makedirs_p()
 
@@ -81,22 +77,27 @@ def main():
     normalize = augmentations.Normalize(mean = [0.45, 0.45, 0.45],
                                         std = [0.225, 0.225, 0.225])
     
+    bright = augmentations.BrightnessContrast(contrast=2., 
+                                              brightness=12.)
+    
     # noise = augmentations.GaussianNoise(noise_factor = 0.25)
 
     train_transform = augmentations.Compose([
         augmentations.RandomHorizontalFlip(),
         augmentations.RandomVerticalFlip(),
+        augmentations.RandomRotation(),
         augmentations.CentreCrop(),
         augmentations.SquareResize(),
-        augmentations.RandomScaleCrop(),
         augmentations.ArrayToTensor(),
         normalize,
         # noise
     ]) if args.chua else augmentations.Compose([
         augmentations.RandomHorizontalFlip(),
         augmentations.RandomVerticalFlip(),
+        augmentations.RandomRotation(),
+        augmentations.CentreCrop(),
         augmentations.SquareResize(),
-        augmentations.RandomScaleCrop(),
+        bright,
         augmentations.ArrayToTensor(),
         normalize,
         # noise
@@ -108,18 +109,24 @@ def main():
         augmentations.ArrayToTensor(),
         normalize
     ]) if args.chua else augmentations.Compose([
+        augmentations.CentreCrop(),
         augmentations.SquareResize(),
+        bright,
         augmentations.ArrayToTensor(),
         normalize,
         # noise
     ])
     
+    if args.recurrency:
+        recurrency_size = 5
+    else:
+        recurrency_size = 1
+
     print("=> Getting scenes from '{}'".format(args.data))
     print("=> Choosing the correct dataset for choice {}...".format(args.train_type))
     
-    train_dataset = ZhongeChuaDataset(args.data, is_train=True, transform=train_transform, seed=args.seed, train_type=args.train_type) if args.chua else VisionStateDataset(args.data, is_train=True, transform=train_transform, seed=args.seed, train_type=args.train_type)
-    val_dataset = ZhongeChuaDataset(args.data, is_train=False, transform=val_transform, seed=args.seed, train_type=args.train_type) if args.chua else VisionStateDataset(args.data, is_train=False, transform=val_transform, seed=args.seed, train_type=args.train_type)
-
+    train_dataset = ZhongeChuaDataset(args.data, is_train=True, transform=train_transform, seed=args.seed, train_type=args.train_type, recurrency_size=recurrency_size) if args.chua else VisionStateDataset(args.data, is_train=True, transform=train_transform, seed=args.seed, train_type=args.train_type, recurrency_size=recurrency_size, load_depths=args.include_depth)
+    val_dataset = ZhongeChuaDataset(args.data, is_train=False, transform=val_transform, seed=args.seed, train_type=args.train_type, recurrency_size=recurrency_size) if args.chua else VisionStateDataset(args.data, is_train=False, transform=val_transform, seed=args.seed, train_type=args.train_type, recurrency_size=recurrency_size, load_depths=args.include_depth)
 
     print('{} samples found in {} train scenes'.format(len(train_dataset), len(train_dataset.folder_index) if args.chua else len(train_dataset.scenes)))
     print('{} samples found in {} validation scenes'.format(len(val_dataset), len(val_dataset.folder_index) if args.chua else len(val_dataset.scenes)))
@@ -132,55 +139,30 @@ def main():
     )
 
     # Create the model
-    
-
     if args.type == "v":
-        include_state = False
+        state_size = 0
     else:
-        include_state = True
+        state_size = 54 if args.chua else 26
 
-    print("=> Creating the {} transformer...".format("vision & state" if include_state else "vision"))
+    print("=> Creating the {} model with {} encoder and {}...".format("vision & state" if args.type=="vs" else "vision", args.architecture, "recurrency" if args.recurrency else "linear"))
 
-    if args.token_sampling:
-        vit_model = ViT(
-                image_size = 256,
-                patch_size = args.patch_size,
-                num_classes = 3,
-                dim = 1024,
-                depth = 6,
-                heads = 16,
-                mlp_dim = 2048,
-                dropout = 0.1,
-                emb_dropout = 0.1,
-                max_tokens_per_depth=(256, 128, 64, 32, 16, 8),
-                state_include = include_state
-        )
-    
-    else:
+    model = ForceEstimator(architecture=args.architecture,
+                           recurrency=args.recurrency,
+                           pretrained=False,
+                           include_depth=args.include_depth,
+                           att_type=args.att_type,
+                           state_size=state_size)
 
-        vit_model = BaseViT(
-                image_size = 256,
-                patch_size = args.patch_size,
-                num_classes = 3,
-                dim = 1024,
-                depth = 6,
-                heads = 16,
-                mlp_dim = 2048,
-                dropout = 0.1,
-                emb_dropout = 0.1,
-                state_include = include_state
-        )
-
-    vit_model.to(device)
+    model.to(device)
 
     #Load parameters
     if args.pretrained:
         print("=> Using pre-trained weights for ViT")
         weights_vit = torch.load(args.pretrained)
-        vit_model.load_state_dict(weights_vit['state_dict'], strict=False)
+        model.load_state_dict(weights_vit['state_dict'], strict=False)
     
     print("=> Setting Adam optimizer")
-    vit_optimizer = torch.optim.Adam(vit_model.parameters(), lr=1e-3 if args.type=="vs" else 1e-4, betas=(args.momentum, args.beta), weight_decay=args.weight_decay)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3 if args.type=="vs" else 1e-4, betas=(args.momentum, args.beta), weight_decay=args.weight_decay)
     
     #Initialize losses
     mse = nn.MSELoss()
@@ -201,12 +183,12 @@ def main():
 
         #train for one epoch
         logger.reset_train_bar()
-        train_loss = train(args, train_loader, vit_model, vit_optimizer, logger, training_writer, mse)
+        train_loss = train(args, train_loader, model, optimizer, logger, training_writer, mse)
         logger.train_writer.write(' * Avg Loss: {:.3f}'.format(train_loss))
         
         #evaluate the model in validation set
         logger.reset_valid_bar()
-        errors, error_names = validate(args, val_loader, vit_model, logger, output_writers, mse=mse)
+        errors, error_names = validate(args, val_loader, model, logger, output_writers)
         error_string = ', '.join('{} : {:.3f}'.format(name, error) for name, error in zip(error_names, errors))
         logger.valid_writer.write(' * Avg {}'.format(error_string))
 
@@ -225,7 +207,7 @@ def main():
         save_checkpoint(
             args.save_path, {
                 'epoch': epoch + 1,
-                'state_dict': vit_model.state_dict(),
+                'state_dict': model.state_dict(),
             },
             is_best)
         
@@ -235,7 +217,7 @@ def main():
     logger.epoch_bar.finish()
 
 
-def train(args: argparse.ArgumentParser.parse_args, train_loader: DataLoader, vit_model: nn.Module, vit_optimizer: torch.optim.Adam, logger: TermLogger, train_writer: SummaryWriter, mse: nn.MSELoss):
+def train(args: argparse.ArgumentParser.parse_args, train_loader: DataLoader, model: nn.Module, optimizer: torch.optim.Adam, logger: TermLogger, train_writer: SummaryWriter, mse: nn.MSELoss):
     global n_iter, device
     batch_time = AverageMeter()
     data_time = AverageMeter()
@@ -244,7 +226,7 @@ def train(args: argparse.ArgumentParser.parse_args, train_loader: DataLoader, vi
     l1_lambda = 1e-3
 
     #switch the vit_models to train mode
-    vit_model.train()
+    model.train()
 
     end = time.time()
     logger.train_bar.update(0)
@@ -254,43 +236,27 @@ def train(args: argparse.ArgumentParser.parse_args, train_loader: DataLoader, vi
             break
         log_losses = i > 0 and n_iter % args.print_freq == 0
         data_time.update(time.time() - end)
-        img = data['img'].to(device)
-        forces = data['forces'].to(device)
+        img = [im.to(device) for im in data['img']] if args.recurrency else data['img'].to(device)
+        forces = data['forces'].to(device).float()
+        state = data['robot_state'].to(device).float() if args.type=="vs" else None           
+        
+        pred_forces = model(state) if args.architecture=="fc" else model(img, state)
+        mse_loss = mse(pred_forces, forces)
+        # Add L1 regularization
+        l1_norm = sum(p.abs().sum() for p in model.parameters())
+        loss = w1 * mse_loss + l1_lambda * l1_norm
 
-        if args.type == 'vs':
-            state = data['robot_state'].to(device)            
-            pred_forces_vit = vit_predict_force_state(vit_model, img, state.unsqueeze(1) if args.chua else state, forces.unsqueeze(1) if args.chua else forces, True)
-            mse_loss_vit = mse(pred_forces_vit, forces)
-            # Add L1 regularization
-            l1_norm_vit = sum(p.abs().sum() for p in vit_model.parameters())
-            loss_vit = w1 * mse_loss_vit + l1_lambda * l1_norm_vit
-
-            if log_losses:
-                train_writer.add_scalar('MSE_ViT', mse_loss_vit.item(), n_iter)
-                train_writer.add_scalar('Loss_ViT', loss_vit.item(), n_iter)
-
-        else:
-            state = None
-            forces = forces if args.chua else forces.mean(axis=1)
-            pred_forces_vit = vit_predict_force_visu(vit_model, img, True)
-            mse_loss_vit = mse(pred_forces_vit, forces)
-
-            # Add L1 regularization
-            l1_norm_vit = sum(p.abs().sum() for p in vit_model.parameters())
-            loss_vit = w1 * mse_loss_vit + l1_lambda * l1_norm_vit
-
-
-            if log_losses:
-                train_writer.add_scalar('MSE_ViT', mse_loss_vit.item(), n_iter)
-                train_writer.add_scalar('Loss_ViT', loss_vit.item(), n_iter)
+        if log_losses:
+            train_writer.add_scalar('MSE_ViT', mse_loss.item(), n_iter)
+            train_writer.add_scalar('Loss_ViT', loss.item(), n_iter)
         
         # record loss and EPE
-        losses.update([loss_vit.item(), mse_loss_vit.item()], args.batch_size)
+        losses.update([loss.item(), mse_loss.item()], args.batch_size)
 
         # compute gradient and do Adam step for vit
-        vit_optimizer.zero_grad()
-        loss_vit.backward()
-        vit_optimizer.step()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
         # measure elapsed time
         batch_time.update(time.time() - end)
@@ -298,10 +264,7 @@ def train(args: argparse.ArgumentParser.parse_args, train_loader: DataLoader, vi
 
         with open(args.save_path/args.log_full, 'a') as csvfile:
             writer = csv.writer(csvfile, delimiter='\t')
-            if args.type == 'vs':
-                writer.writerow([loss_vit.item(), mse_loss_vit.item()])
-            else:
-                writer.writerow([loss_vit.item(), mse_loss_vit.item()])
+            writer.writerow([loss.item(), mse_loss.item()])
 
         logger.train_bar.update(i+1)
         if i % args.print_freq == 0:
@@ -312,34 +275,26 @@ def train(args: argparse.ArgumentParser.parse_args, train_loader: DataLoader, vi
     return losses.avg[1]
 
 @torch.no_grad()
-def validate(args:argparse.ArgumentParser.parse_args, val_loader: DataLoader, vit_model: nn.Module, logger: TermLogger, output_writers: SummaryWriter = [], mse = None):
-    global devic
+def validate(args:argparse.ArgumentParser.parse_args, val_loader: DataLoader, model: nn.Module, logger: TermLogger, output_writers: SummaryWriter = []):
+    global device
     batch_time = AverageMeter()
     losses = AverageMeter(i=1, precision=4)
     log_outputs = len(output_writers) > 0
 
     #switch to evaluate mode
-    vit_model.eval()
+    model.eval()
 
     end = time.time()
     logger.valid_bar.update(0)
 
     for i, data in enumerate(val_loader):
-        img = data['img'].to(device)
-        forces = data['forces'].to(device)
-
-        if args.type == 'vs':
-            state = data['robot_state'].to(device)            
-            vit_pred_forces = vit_predict_force_state(vit_model, img, state.unsqueeze(1) if args.chua else state, forces.unsqueeze(1) if args.chua else forces, True)
-            vit_loss = torch.sqrt(((forces - vit_pred_forces) ** 2).mean())
-            losses.update([vit_loss.item()])
-
-        else:
-            state = None
-            forces = forces if args.chua else forces.mean(axis=1)
-            vit_pred_forces = vit_predict_force_visu(vit_model, img, True)
-            vit_loss = torch.sqrt(((forces - vit_pred_forces) ** 2).mean())
-            losses.update([vit_loss.item()])
+        img = [im.to(device) for im in data['img']] if args.recurrency else data['img'].to(device)
+        forces = data['forces'].to(device).float()
+        state = data['robot_state'].to(device).float() if args.type == "vs" else None            
+        
+        pred_forces = model(state) if args.architecture=="fc" else model(img, state)
+        loss = torch.sqrt(((forces - pred_forces) ** 2).mean())
+        losses.update([loss.item()])
         
         # measure elapsed time
         batch_time.update(time.time() - end)
@@ -349,35 +304,7 @@ def validate(args:argparse.ArgumentParser.parse_args, val_loader: DataLoader, vi
             logger.valid_writer.write('Valid: Time {} Loss {}'.format(batch_time, losses))
     
     logger.valid_bar.update(len(val_loader))
-    return losses.avg, ['ViT Loss']
-
-
-def vit_predict_force_state(model, images, state, forces, is_train = True):
-    preds_forces = torch.zeros(*forces.shape).to(device)
-
-    if is_train:
-        sampled_token_ids = False
-    else:
-        sampled_token_ids = True
-    
-    for i in range(state.shape[1]):
-        if not sampled_token_ids:
-            preds_forces[:, i, :] = model(images, sampled_token_ids, state[:, i, :].unsqueeze(1))
-        else:
-            preds_forces[:, i, :], token_ids = model(images, sampled_token_ids, state[:, i, :].unsqueeze(1))
-    
-    return preds_forces if is_train else (preds_forces, token_ids)
-
-def vit_predict_force_visu(model, images, is_train = True):
-
-    if is_train:
-        sampled_token_ids = False
-        pred_forces = model(images, sampled_token_ids, None)
-    else:
-        sampled_token_ids = True
-        pred_forces, token_ids = model(images, sampled_token_ids, None)
-    
-    return pred_forces if is_train else (pred_forces, token_ids)
+    return losses.avg, ['Loss']
 
     
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ from torch.utils.data import Dataset
 from path import Path
 import numpy as np
 import torch.nn as nn
+import datetime
 
 def save_checkpoint(save_path: Path, model_state, is_best:bool, filename='checkpoint.pth.tar'):
     torch.save(model_state, save_path/'{}'.format(filename))
@@ -13,116 +14,23 @@ def save_checkpoint(save_path: Path, model_state, is_best:bool, filename='checkp
         shutil.copyfile(save_path/'{}'.format(filename),
                         save_path/'model_best.pth.tar')
 
-def dtw(s, t):
-    n, m = len(s), len(t)
-    dtw_matrix = np.zeros((n+1, m+1))
-    for i in range(n+1):
-        for j in range(m+1):
-            dtw_matrix[i, j] = np.inf
-    dtw_matrix[0, 0] = 0
+def none_or_str(value):
+    if value=="None":
+        return None
+    return value
 
-    for i in range(1, n+1):
-        for j in range(1, m+1):
-            cost = abs(s[i - 1] - t[j - 1])
-            last_min = np.min([dtw_matrix[i - 1 , j], dtw_matrix[i, j - 1], dtw_matrix[i - 1, j - 1]])
-            dtw_matrix[i, j] = cost + last_min
+def create_saving_dir(root: Path, experiment_name: str, architecture: str, include_depth: bool, chua: bool, recurrency: bool, att_type: str = None):
+    timestamp = datetime.datetime.now().strftime("%m-%d-%H:%M")
+
+    if recurrency:
+        architecture = "r" + architecture
     
-    return dtw_matrix
-
-__all__ = ['frdist']
-
-
-def _c(ca, i, j, p, q):
-
-    if ca[i, j] > -1:
-        return ca[i, j]
-    elif i == 0 and j == 0:
-        ca[i, j] = np.linalg.norm(p[i]-q[j])
-    elif i > 0 and j == 0:
-        ca[i, j] = max(_c(ca, i-1, 0, p, q), np.linalg.norm(p[i]-q[j]))
-    elif i == 0 and j > 0:
-        ca[i, j] = max(_c(ca, 0, j-1, p, q), np.linalg.norm(p[i]-q[j]))
-    elif i > 0 and j > 0:
-        ca[i, j] = max(
-            min(
-                _c(ca, i-1, j, p, q),
-                _c(ca, i-1, j-1, p, q),
-                _c(ca, i, j-1, p, q)
-            ),
-            np.linalg.norm(p[i]-q[j])
-            )
+    if att_type is not None:
+        architecture = architecture + att_type.lower()
+    
+    if architecture == "fc":
+        save_path = root/"{}".format("chua" if chua else "img2force")/architecture/experiment_name/timestamp
     else:
-        ca[i, j] = float('inf')
-
-    return ca[i, j]
-
-
-def frdist(p, q):
-    """
-    Computes the discrete Fréchet distance between
-    two curves. The Fréchet distance between two curves in a
-    metric space is a measure of the similarity between the curves.
-    The discrete Fréchet distance may be used for approximately computing
-    the Fréchet distance between two arbitrary curves,
-    as an alternative to using the exact Fréchet distance between a polygonal
-    approximation of the curves or an approximation of this value.
-    This is a Python 3.* implementation of the algorithm produced
-    in Eiter, T. and Mannila, H., 1994. Computing discrete Fréchet distance.
-    Tech. Report CD-TR 94/64, Information Systems Department, Technical
-    University of Vienna.
-    http://www.kr.tuwien.ac.at/staff/eiter/et-archive/cdtr9464.pdf
-    Function dF(P, Q): real;
-        input: polygonal curves P = (u1, . . . , up) and Q = (v1, . . . , vq).
-        return: δdF (P, Q)
-        ca : array [1..p, 1..q] of real;
-        function c(i, j): real;
-            begin
-                if ca(i, j) > −1 then return ca(i, j)
-                elsif i = 1 and j = 1 then ca(i, j) := d(u1, v1)
-                elsif i > 1 and j = 1 then ca(i, j) := max{ c(i − 1, 1), d(ui, v1) }
-                elsif i = 1 and j > 1 then ca(i, j) := max{ c(1, j − 1), d(u1, vj) }
-                elsif i > 1 and j > 1 then ca(i, j) :=
-                max{ min(c(i − 1, j), c(i − 1, j − 1), c(i, j − 1)), d(ui, vj ) }
-                else ca(i, j) = ∞
-                return ca(i, j);
-            end; /* function c */
-        begin
-            for i = 1 to p do for j = 1 to q do ca(i, j) := −1.0;
-            return c(p, q);
-        end.
-    Parameters
-    ----------
-    P : Input curve - two dimensional array of points
-    Q : Input curve - two dimensional array of points
-    Returns
-    -------
-    dist: float64
-        The discrete Fréchet distance between curves `P` and `Q`.
-    Examples
-    --------
-    >>> from frechetdist import frdist
-    >>> P=[[1,1], [2,1], [2,2]]
-    >>> Q=[[2,2], [0,1], [2,4]]
-    >>> frdist(P,Q)
-    >>> 2.0
-    >>> P=[[1,1], [2,1], [2,2]]
-    >>> Q=[[1,1], [2,1], [2,2]]
-    >>> frdist(P,Q)
-    >>> 0
-    """
-    p = np.array(p, np.float64)
-    q = np.array(q, np.float64)
-
-    len_p = len(p)
-    len_q = len(q)
-
-    if len_p == 0 or len_q == 0:
-        raise ValueError('Input curves are empty.')
-
-    if len_p != len_q or len(p[0]) != len(q[0]):
-        raise ValueError('Input curves do not have the same dimensions.')
-
-    ca = (np.ones((len_p, len_q), dtype=np.float64) * -1)
-
-    dist = _c(ca, len_p-1, len_q-1, p, q)
-    return dist
+        save_path = root/"{}/{}".format("chua" if chua else "img2force", "rgbd" if include_depth else "rgb")/architecture/experiment_name/timestamp
+    
+    return save_path
